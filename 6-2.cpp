dcbda4ac50f963d78db65497ae9e64725846d31c
varunbw <varunbalawade@gmail.com>
2025-01-01 18:02:14 +0530

    6

---
+++ 6-2.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+pair<int, int> GetStartingPosition(vector<vector<char>>& vec) {
+
+    int iRes = 0;
+    int jRes = 0;
+
+    const int m = vec.size();
+    const int n = vec[0].size();
+
+    for (int i = 0; i < m && iRes == 0; i++) {
+        for (int j = 0; j < n && jRes == 0; j++) {
+            if (vec[i][j] == '^') {
+                iRes = i;
+                jRes = j;
+                break;
+            }
+        }
+    }
+
+    return make_pair(iRes, jRes);
+}
+
+bool WithinBounds(int i, int j, int m, int n) {
+    return i >= 0 && j >= 0 && i < m && j < n;
+}
+
+bool IsStuckInLoop(vector<vector<char>>& vec) {
+
+    auto const [iStart, jStart] = GetStartingPosition(vec);
+
+    // Up, Down, Left, Right (U, D, L, R respectively)
+    char dir = 'U';
+
+    const int m = vec.size();
+    const int n = vec[0].size();
+
+    int i = iStart;
+    int j = jStart;
+    int ctr = 0;
+
+    while (WithinBounds(i, j, m, n) && ctr++ < m * n) {
+        switch (dir) {
+            case 'U':
+                if (WithinBounds(i - 1, j, m, n) && vec[i - 1][j] == '#') 
+                    dir = 'R';
+                else 
+                    i--;
+                break;
+
+            case 'D':
+                if (WithinBounds(i + 1, j, m, n) && vec[i + 1][j] == '#') 
+                    dir = 'L';
+                else 
+                    i++;
+                break;
+            
+            case 'L':
+                if (WithinBounds(i, j - 1, m, n) && vec[i][j - 1] == '#') 
+                    dir = 'U';
+                else 
+                    j--;
+                break;
+
+            case 'R':
+                if (WithinBounds(i, j + 1, m, n) && vec[i][j + 1] == '#') 
+                    dir = 'D';
+                else 
+                    j++;
+                break;
+
+            default:
+                cout << "[????????]: How did we get here\n";
+        }
+    }
+
+    return ctr >= m * n;
+}
+
+
+// ! This solution is so bad
+unsigned long long solve(vector<vector<char>>& vec) {
+
+    const int m = vec.size();
+    const int n = vec[0].size();
+
+    unsigned long long res = 0;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            char old = vec[i][j];
+            if (old == '#')
+                continue;
+
+            vec[i][j] = '#';
+            res += IsStuckInLoop(vec);
+            vec[i][j] = old;            
+        }
+    }
+
+    return res;
+}
+
+int main(void) {
+
+    string line;
+
+    vector<vector<char>> vec;
+    while (getline(fin, line)) {
+        vector<char> row;
+        row.reserve(line.size());
+        for (char c : line) {
+            row.push_back(c);
+        }
+
+        vec.push_back(row);
+    }
+
+    unsigned long long res = solve(vec);
+    cout << res;
+
+    cout << endl;
+    return 0;
+}
